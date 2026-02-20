5d7d0a4da8e06a89dd856dd9cbe321244b107745
rinshan77 <175357996+rinshan77@users.noreply.github.com>
2026-02-21 00:02:12 +0100

    added tests

---
+++ src/repl.test.ts
@@ -0,0 +1,34 @@
+import { cleanInput } from "./repl";
+import { describe, expect, test } from "vitest";
+
+describe.each([
+  {
+    input: "  hello  world  ",
+    expected: ["hello", "world"],
+  },
+  {
+    input: "Charmander Bulbasaur PIKACHU",
+    expected: ["charmander", "bulbasaur", "pikachu"],
+  },
+  {
+    input: "   squirtle   ",
+    expected: ["squirtle"],
+  },
+  {
+    input: "",
+    expected: [],
+  },
+  {
+    input: "   ",
+    expected: [],
+  },
+])("cleanInput($input)", ({ input, expected }) => {
+  test(`Expected: ${expected}`, () => {
+    const actual = cleanInput(input);
+
+    expect(actual).toHaveLength(expected.length);
+    for (const i in expected) {
+      expect(actual[i]).toBe(expected[i]);
+    }
+  });
+});
